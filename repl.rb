e1157f4613509e788bc2ebdaf0ad0ac7b0cc5a8e
betogls <betogls02@gmail.com>
2018-03-03 04:28:43 -0700

    update

--- repl.rb
+++ repl.rb
@@ -1,81 +1,67 @@
 #!/usr/bin/env ruby
 
-def opt_file(input,hash = "")
-    out_file = File.new("out.rb", "w")
-    out_file.puts("#!/usr/bin/env ruby")
-    if hash != ''
-      hash.each {|key,value| out_file.puts("#{key} = #{value}")}
-    end
-    out_file.puts("print #{input}")
-    File.chmod(0777,"out.rb")
-    out_file.close
-    return `ruby out.rb`
-end
-
-#def repl(input = "")
 variables = {}
 while true do
   print "REPL >> "
   input = gets.chomp
   case input
-  when /\s*[a-zA-Z]\w*\s*=/
-    input_arr=input.split('=')
-    vars_arr = input_arr[1].split(%r{[()-+/*]})
-    vars_arr.delete("")
-    defined = true
-    vars_arr.each do |element|
-      unless element =~ /^\s*(\d|"(\w|\s)*")\s*$/ || variables.key?(element)
-        puts "undefined local variable or method #{element}"
-	defined = false
-      end
-    end
-    variables[input_arr[0]] = input_arr[1] if defined == true
 
-  when /print/
-    print  opt_file(input,variables)
+  when /\s*[a-zA-Z]\w*\s*=/ # -- assign
+	    input = input.gsub!(/"/,"\\\"") if input =~ /"/
+	    input_arr=input.split('=')
+	    vars_arr = input_arr[1].split(%r{[()-+/*]})
+	    vars_arr.delete("")
+	    defined = true
+	    vars_arr.each do |var|
+	      unless var =~ /^\s*(\d+|\\"(\w|\s)*\\")\s*$/ || variables.key?(var)
+	        puts "undefined local variable or method #{var}"
+		defined = false
+	      end
+	    end
+	    variables[input_arr[0].delete(" ")] = input_arr[1] if defined == true
 
-  when /puts/
-    puts  opt_file(input,variables)
-
-  when /p/
-    p  opt_file(input,variables)
-
-  when /\+|-|\/|\*|==|>|>=|<|<=|!=|\(|\)|\[|\]|\{|\}/ 
-    puts  opt_file(input,variables)
- 
   when "quit", "exit"
-    `rm -rf out.rb`
-    puts "good bye!"
-    abort
+	    puts "good bye!"
+	    abort
   when "pwd"
-    puts Dir.pwd
+	    puts Dir.pwd
 
   when /ls/
-    files=Dir.entries(Dir.pwd)
-    files.each {|file| puts "  "+file}
-
+	    files=Dir.entries(Dir.pwd)
+	    files.each {|file| puts "  "+file}
+	
   when /cd/ 
-    input=input.split(" ")
-    Dir.chdir(input[-1])
-    puts "#{Dir.pwd}"
+	    input=input.split(" ")
+	    Dir.chdir(input[-1])
+	    puts "#{Dir.pwd}"
 
   when /mkdir/ 
-    input=input.split(" ")
-    input.shift
-    input.each do |dir|
-      Dir.mkdir(dir)
-      puts "directory #{dir} created"
-    end
+	    input=input.split(" ")
+	    input.shift
+	    input.each do |dir|
+	      Dir.mkdir(dir)
+	      puts "directory #{dir} created"
+            end
 
   when /rm/
-    input=input.split(" ")
-    input.shift
-    input.each do |dir|
-      Dir.delete(dir)
-      puts "removed directory #{dir}"
-    end
-  end
-end
-#end
+	    input=input.split(" ")
+	    input.shift
+	    input.each do |dir|
+	      Dir.delete(dir)
+	      puts "removed directory #{dir}"
+	    end
+  else
+	input.sub!(/(puts|print)\s*/,"")
+	input_arr = input.split(/(\+|-|\/|\*|==|>|>=|<|<=|!=|\(|\)|\[|\]|\#{|})/)
+	input = "puts "
+	input_arr.each do |element|
+	  element.gsub!(/"/,"\\\"") if element =~ /"/
+	  input <<  element
+	end
+	vars=""
+	variables.each {|x,y| vars << "#{x} = #{y};"}
+	input.insert(0,vars)
+	puts `ruby -e "#{input}"`
 
-#repl()
+  end 
+end
